8f3f4fdde7cee227181f70da0142fa4d47ebfd8c
Đạt Trương <dat82770@gmail.com>
2026-02-23 21:14:50 +0700

    feat(schema): add initial database schema (#16)
    * feat: add schema image ERD
    
    * feat(schema): add initial database schema

---
+++ schema/db_web.sql
@@ -0,0 +1,576 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 23, 2026 at 03:12 PM
+-- Server version: 8.0.44
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_web`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_LayChiTietSanPham` (IN `p_san_pham_id` INT)   BEGIN
+    SELECT 
+        sp.ten_san_pham, 
+        sp.hang_san_xuat,
+        pb.sku, 
+        pb.ten_phien_ban, 
+        pb.mau_sac, 
+        pb.cau_hinh, 
+        pb.gia_ban, 
+        pb.so_luong_ton
+    FROM SAN_PHAM sp
+    JOIN PHIEN_BAN_SAN_PHAM pb ON sp.id = pb.san_pham_id
+    WHERE sp.id = p_san_pham_id;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chi_tiet_don`
+--
+
+CREATE TABLE `chi_tiet_don` (
+  `id` int NOT NULL,
+  `don_hang_id` int NOT NULL,
+  `phien_ban_id` int NOT NULL,
+  `so_luong` int DEFAULT '1',
+  `gia_tai_thoi_diem_mua` decimal(15,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chi_tiet_gio`
+--
+
+CREATE TABLE `chi_tiet_gio` (
+  `id` int NOT NULL,
+  `gio_hang_id` int NOT NULL,
+  `phien_ban_id` int NOT NULL,
+  `so_luong` int DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danh_gia`
+--
+
+CREATE TABLE `danh_gia` (
+  `id` int NOT NULL,
+  `nguoi_dung_id` int NOT NULL,
+  `san_pham_id` int NOT NULL,
+  `so_sao` int DEFAULT NULL,
+  `noi_dung` text COLLATE utf8mb4_unicode_ci,
+  `ngay_viet` datetime DEFAULT CURRENT_TIMESTAMP
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danh_muc`
+--
+
+CREATE TABLE `danh_muc` (
+  `id` int NOT NULL,
+  `ten` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `danh_muc_cha_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dia_chi`
+--
+
+CREATE TABLE `dia_chi` (
+  `id` int NOT NULL,
+  `nguoi_dung_id` int NOT NULL,
+  `ten_nguoi_nhan` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sdt_nhan` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `so_nha_duong` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phuong_xa` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `quan_huyen` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tinh_thanh` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mac_dinh` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `don_hang`
+--
+
+CREATE TABLE `don_hang` (
+  `id` int NOT NULL,
+  `nguoi_dung_id` int DEFAULT NULL COMMENT 'Nullable (null neu la guest)',
+  `dia_chi_id` int DEFAULT NULL COMMENT 'Nullable (neu login)',
+  `trang_thai` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT 'CHO_DUYET' COMMENT 'CHO_DUYET, DA_HUY...',
+  `tong_tien` decimal(15,2) DEFAULT NULL,
+  `thong_tin_guest` text COLLATE utf8mb4_unicode_ci COMMENT 'JSON: ten, sdt, dia chi',
+  `ngay_tao` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gio_hang`
+--
+
+CREATE TABLE `gio_hang` (
+  `id` int NOT NULL,
+  `nguoi_dung_id` int DEFAULT NULL COMMENT 'Nullable',
+  `session_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Cho khach vang lai'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khuyen_mai`
+--
+
+CREATE TABLE `khuyen_mai` (
+  `id` int NOT NULL,
+  `ten_chuong_trinh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ngay_bat_dau` datetime DEFAULT NULL,
+  `ngay_ket_thuc` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lich_su_tim_kiem`
+--
+
+CREATE TABLE `lich_su_tim_kiem` (
+  `id` int NOT NULL,
+  `nguoi_dung_id` int NOT NULL,
+  `tu_khoa` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `thoi_gian_tim` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoi_dung`
+--
+
+CREATE TABLE `nguoi_dung` (
+  `id` int NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mat_khau` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ho_ten` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sdt` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `loai_tai_khoan` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT 'MEMBER' COMMENT 'ADMIN, MEMBER'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phien_ban_san_pham`
+--
+
+CREATE TABLE `phien_ban_san_pham` (
+  `id` int NOT NULL,
+  `san_pham_id` int NOT NULL,
+  `sku` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ten_phien_ban` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mau_sac` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cau_hinh` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gia_ban` decimal(15,2) DEFAULT NULL,
+  `so_luong_ton` int DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `san_pham`
+--
+
+CREATE TABLE `san_pham` (
+  `id` int NOT NULL,
+  `danh_muc_id` int DEFAULT NULL,
+  `ten_san_pham` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hang_san_xuat` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mo_ta` text COLLATE utf8mb4_unicode_ci,
+  `diem_danh_gia` float DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `san_pham_khuyen_mai`
+--
+
+CREATE TABLE `san_pham_khuyen_mai` (
+  `san_pham_id` int NOT NULL,
+  `khuyen_mai_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thanh_toan`
+--
+
+CREATE TABLE `thanh_toan` (
+  `id` int NOT NULL,
+  `don_hang_id` int NOT NULL,
+  `nguoi_duyet_id` int DEFAULT NULL COMMENT 'Admin ID',
+  `phuong_thuc` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'COD, QR, TRA_GOP',
+  `so_tien` decimal(15,2) DEFAULT NULL,
+  `anh_bien_lai` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'URL anh',
+  `trang_thai_duyet` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT 'CHO_DUYET' COMMENT 'CHO_DUYET, THANH_CONG',
+  `ngay_thanh_toan` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thong_so_ky_thuat`
+--
+
+CREATE TABLE `thong_so_ky_thuat` (
+  `id` int NOT NULL,
+  `san_pham_id` int NOT NULL,
+  `ten_thong_so` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Ram, Chip, Pin...',
+  `gia_tri` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '8GB, M1, 5000mAh...'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `yeu_thich`
+--
+
+CREATE TABLE `yeu_thich` (
+  `nguoi_dung_id` int NOT NULL,
+  `san_pham_id` int NOT NULL,
+  `ngay_them` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chi_tiet_don`
+--
+ALTER TABLE `chi_tiet_don`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `don_hang_id` (`don_hang_id`),
+  ADD KEY `phien_ban_id` (`phien_ban_id`);
+
+--
+-- Indexes for table `chi_tiet_gio`
+--
+ALTER TABLE `chi_tiet_gio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gio_hang_id` (`gio_hang_id`),
+  ADD KEY `phien_ban_id` (`phien_ban_id`);
+
+--
+-- Indexes for table `danh_gia`
+--
+ALTER TABLE `danh_gia`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nguoi_dung_id` (`nguoi_dung_id`),
+  ADD KEY `san_pham_id` (`san_pham_id`);
+
+--
+-- Indexes for table `danh_muc`
+--
+ALTER TABLE `danh_muc`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `danh_muc_cha_id` (`danh_muc_cha_id`);
+
+--
+-- Indexes for table `dia_chi`
+--
+ALTER TABLE `dia_chi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nguoi_dung_id` (`nguoi_dung_id`);
+
+--
+-- Indexes for table `don_hang`
+--
+ALTER TABLE `don_hang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nguoi_dung_id` (`nguoi_dung_id`),
+  ADD KEY `dia_chi_id` (`dia_chi_id`);
+
+--
+-- Indexes for table `gio_hang`
+--
+ALTER TABLE `gio_hang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nguoi_dung_id` (`nguoi_dung_id`);
+
+--
+-- Indexes for table `khuyen_mai`
+--
+ALTER TABLE `khuyen_mai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lich_su_tim_kiem`
+--
+ALTER TABLE `lich_su_tim_kiem`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nguoi_dung_id` (`nguoi_dung_id`);
+
+--
+-- Indexes for table `nguoi_dung`
+--
+ALTER TABLE `nguoi_dung`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `phien_ban_san_pham`
+--
+ALTER TABLE `phien_ban_san_pham`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `san_pham_id` (`san_pham_id`);
+
+--
+-- Indexes for table `san_pham`
+--
+ALTER TABLE `san_pham`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `danh_muc_id` (`danh_muc_id`);
+
+--
+-- Indexes for table `san_pham_khuyen_mai`
+--
+ALTER TABLE `san_pham_khuyen_mai`
+  ADD PRIMARY KEY (`san_pham_id`,`khuyen_mai_id`),
+  ADD KEY `khuyen_mai_id` (`khuyen_mai_id`);
+
+--
+-- Indexes for table `thanh_toan`
+--
+ALTER TABLE `thanh_toan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `don_hang_id` (`don_hang_id`),
+  ADD KEY `nguoi_duyet_id` (`nguoi_duyet_id`);
+
+--
+-- Indexes for table `thong_so_ky_thuat`
+--
+ALTER TABLE `thong_so_ky_thuat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `san_pham_id` (`san_pham_id`);
+
+--
+-- Indexes for table `yeu_thich`
+--
+ALTER TABLE `yeu_thich`
+  ADD PRIMARY KEY (`nguoi_dung_id`,`san_pham_id`),
+  ADD KEY `san_pham_id` (`san_pham_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chi_tiet_don`
+--
+ALTER TABLE `chi_tiet_don`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chi_tiet_gio`
+--
+ALTER TABLE `chi_tiet_gio`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `danh_gia`
+--
+ALTER TABLE `danh_gia`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `danh_muc`
+--
+ALTER TABLE `danh_muc`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dia_chi`
+--
+ALTER TABLE `dia_chi`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `don_hang`
+--
+ALTER TABLE `don_hang`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gio_hang`
+--
+ALTER TABLE `gio_hang`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `khuyen_mai`
+--
+ALTER TABLE `khuyen_mai`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lich_su_tim_kiem`
+--
+ALTER TABLE `lich_su_tim_kiem`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nguoi_dung`
+--
+ALTER TABLE `nguoi_dung`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `phien_ban_san_pham`
+--
+ALTER TABLE `phien_ban_san_pham`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `san_pham`
+--
+ALTER TABLE `san_pham`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `thanh_toan`
+--
+ALTER TABLE `thanh_toan`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `thong_so_ky_thuat`
+--
+ALTER TABLE `thong_so_ky_thuat`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chi_tiet_don`
+--
+ALTER TABLE `chi_tiet_don`
+  ADD CONSTRAINT `chi_tiet_don_ibfk_1` FOREIGN KEY (`don_hang_id`) REFERENCES `don_hang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `chi_tiet_don_ibfk_2` FOREIGN KEY (`phien_ban_id`) REFERENCES `phien_ban_san_pham` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `chi_tiet_gio`
+--
+ALTER TABLE `chi_tiet_gio`
+  ADD CONSTRAINT `chi_tiet_gio_ibfk_1` FOREIGN KEY (`gio_hang_id`) REFERENCES `gio_hang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `chi_tiet_gio_ibfk_2` FOREIGN KEY (`phien_ban_id`) REFERENCES `phien_ban_san_pham` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `danh_gia`
+--
+ALTER TABLE `danh_gia`
+  ADD CONSTRAINT `danh_gia_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `danh_gia_ibfk_2` FOREIGN KEY (`san_pham_id`) REFERENCES `san_pham` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `danh_muc`
+--
+ALTER TABLE `danh_muc`
+  ADD CONSTRAINT `danh_muc_ibfk_1` FOREIGN KEY (`danh_muc_cha_id`) REFERENCES `danh_muc` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `dia_chi`
+--
+ALTER TABLE `dia_chi`
+  ADD CONSTRAINT `dia_chi_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `don_hang`
+--
+ALTER TABLE `don_hang`
+  ADD CONSTRAINT `don_hang_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `don_hang_ibfk_2` FOREIGN KEY (`dia_chi_id`) REFERENCES `dia_chi` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `gio_hang`
+--
+ALTER TABLE `gio_hang`
+  ADD CONSTRAINT `gio_hang_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `lich_su_tim_kiem`
+--
+ALTER TABLE `lich_su_tim_kiem`
+  ADD CONSTRAINT `lich_su_tim_kiem_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `phien_ban_san_pham`
+--
+ALTER TABLE `phien_ban_san_pham`
+  ADD CONSTRAINT `phien_ban_san_pham_ibfk_1` FOREIGN KEY (`san_pham_id`) REFERENCES `san_pham` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `san_pham`
+--
+ALTER TABLE `san_pham`
+  ADD CONSTRAINT `san_pham_ibfk_1` FOREIGN KEY (`danh_muc_id`) REFERENCES `danh_muc` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `san_pham_khuyen_mai`
+--
+ALTER TABLE `san_pham_khuyen_mai`
+  ADD CONSTRAINT `san_pham_khuyen_mai_ibfk_1` FOREIGN KEY (`san_pham_id`) REFERENCES `san_pham` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `san_pham_khuyen_mai_ibfk_2` FOREIGN KEY (`khuyen_mai_id`) REFERENCES `khuyen_mai` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `thanh_toan`
+--
+ALTER TABLE `thanh_toan`
+  ADD CONSTRAINT `thanh_toan_ibfk_1` FOREIGN KEY (`don_hang_id`) REFERENCES `don_hang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `thanh_toan_ibfk_2` FOREIGN KEY (`nguoi_duyet_id`) REFERENCES `nguoi_dung` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `thong_so_ky_thuat`
+--
+ALTER TABLE `thong_so_ky_thuat`
+  ADD CONSTRAINT `thong_so_ky_thuat_ibfk_1` FOREIGN KEY (`san_pham_id`) REFERENCES `san_pham` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `yeu_thich`
+--
+ALTER TABLE `yeu_thich`
+  ADD CONSTRAINT `yeu_thich_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `yeu_thich_ibfk_2` FOREIGN KEY (`san_pham_id`) REFERENCES `san_pham` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
