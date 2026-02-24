73e707a5428480d96a31642e89155e5e48ea2f33
NuTan KIT <hsntk1610@gmail.com>
2026-02-24 14:49:09 +0700

    add

--- procedures/DH_TaoDonHang.sql
+++ procedures/DH_TaoDonHang.sql
@@ -3,11 +3,18 @@ BEGIN
         nguoi_dung_id,
         dia_chi_id,
         trang_thai,
-        tong_tien
+        tong_tien,
+        thong_tin_guest,
+        ngay_tao
     )
     VALUES(
         p_nguoi_dung_id,
         p_dia_chi_id,
-        0
+        0,
+        0,
+        NULL,
+        NOW()
     );
+    -- lấy id đơn vừa tạo
+    SET p_don_hang_id = LAST_INSERT_ID();
 END
--- procedures/DH_ThemSanPhamVaoDonHang.sql
+++ procedures/DH_ThemSanPhamVaoDonHang.sql
@@ -1,4 +1,5 @@
 BEGIN
+    -- thêm chi tiết đơn
     INSERT INTO chi_tiet_don(
         don_hang_id,
         phien_ban_id,
@@ -13,5 +14,22 @@ BEGIN
     FROM gio_hang gh
     JOIN chi_tiet_gio ct ON gh.id = ct.gio_hang_id
     JOIN phien_ban_san_pham pb ON pb.id = ct.phien_ban_id
+    WHERE gh.nguoi_dung_id = p_nguoi_dung_id;
+    -- cập nhật tổng tiền
+    UPDATE don_hang dh
+    SET tong_tien = (
+        SELECT SUM(so_luong * gia_tai_thoi_diem_mua)
+        FROM chi_tiet_don
+        WHERE don_hang_id = p_don_hang_id
+    )
+    WHERE dh.id = p_don_hang_id;
+    -- trừ tồn kho
+    UPDATE phien_ban_san_pham pb
+    JOIN chi_tiet_don ct ON pb.id = ct.phien_ban_id
+    SET pb.so_luong_ton = pb.so_luong_ton - ct.so_luong
+    WHERE ct.don_hang_id = p_don_hang_id;
+    -- xoá giỏ
+    DELETE ct FROM chi_tiet_gio ct
+    JOIN gio_hang gh ON gh.id = ct.gio_hang_id
     WHERE gh.nguoi_dung_id = p_nguoi_dung_id;
 END
