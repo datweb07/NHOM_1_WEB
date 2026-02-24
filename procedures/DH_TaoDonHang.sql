458ebc885342915cc18c719b2512f035902aefe2
NuTan KIT <hsntk1610@gmail.com>
2026-02-24 23:00:52 +0700

    feat (procedure): add new DonHang (#28)
    * Thêm procedure thêm sản phẩm vào đơn hàng
    
    * Create an order
    
    * add

--- procedures/DH_TaoDonHang.sql
+++ procedures/DH_TaoDonHang.sql
@@ -3,11 +3,8 @@ BEGIN
         nguoi_dung_id,
         dia_chi_id,
         trang_thai,
-        tong_tien
     )
     VALUES(
         p_nguoi_dung_id,
         p_dia_chi_id,
-        0
-    );
 END
