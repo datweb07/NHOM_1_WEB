881d7cd37a3d7b7d9c28c1364cd9db41ea113dd7
datweb07 <dat82770@gmail.com>
2026-03-03 12:38:08 +0700

    schema: update SP_LayChiTietPhienBanSanPham to support new schema fields
    Co-Authored-By: NuTan KIT <200808933+nguyentankhiem1610@users.noreply.github.com>

--- procedures/SP_LayChiTietSanPham.sql
+++ procedures/SP_LayChiTietSanPham.sql
@@ -5,9 +5,13 @@ BEGIN
         pb.sku, 
         pb.ten_phien_ban, 
         pb.mau_sac, 
+        pb.dung_luong,
+        pb.ram,
         pb.cau_hinh, 
+        p.gia_goc,
         pb.gia_ban, 
-        pb.so_luong_ton
+        pb.so_luong_ton,
+        pb.trang_thai
     FROM SAN_PHAM sp
     JOIN PHIEN_BAN_SAN_PHAM pb ON sp.id = pb.san_pham_id
     WHERE sp.id = p_san_pham_id;
