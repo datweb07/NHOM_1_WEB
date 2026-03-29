204e9e7c79c67c3297716e8f73b7b4d7254d4110
datweb07 <dat82770@gmail.com>
2026-03-30 00:56:20 +0700

    feat: refactor UI profile and adjust layout pattern

--- app/views/client/layouts/footer.php
+++ app/views/client/layouts/footer.php
@@ -1,86 +1,71 @@
-<!-- footer -->
-<div class="footer">
-    <div class="grid wide">
-        <div class="row">
+<footer class="bg-light pt-5 pb-3 mt-5">
+    <div class="container-xl">
+        <div class="row g-4">
 
-            <div class="col l-2 m-4 c-12">
-                <div class="footer-1">
-                    <ul class="footer-list">
-                        <li class="footer-list-item"><a href="#">Giới thiệu về công ty</a></li>
-                        <li class="footer-list-item"><a href="#">Câu hỏi thường gặp mua hàng</a></li>
-                        <li class="footer-list-item"><a href="#">Chính sách bảo mật</a></li>
-                        <li class="footer-list-item"><a href="#">Quy chế hoạt động</a></li>
-                        <li class="footer-list-item"><a href="#">Kiểm tra hóa đơn điện tử</a></li>
-                    </ul>
+            <div class="col-lg-2 col-md-4 col-6">
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Giới thiệu về công ty</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Câu hỏi thường gặp mua hàng</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Chính sách bảo mật</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Quy chế hoạt động</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Kiểm tra hóa đơn điện tử</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-6">
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Tin tuyển dụng</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Tin khuyến mãi</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Hướng dẫn mua online</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Hướng dẫn mua trả góp</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Chính sách trả góp</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-6">
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Hệ thống cửa hàng</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Bán hàng doanh nghiệp</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Hệ thống bảo hành</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Giới thiệu máy đổi trả</a></li>
+                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Chính sách đổi trả</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 col-12">
+                <p class="fw-bold small mb-1">Tư vấn mua hàng (Miễn phí)</p>
+                <p class="text-danger fw-bold mb-3">1800 6601</p>
+                <p class="fw-bold small mb-1">Hỗ trợ kỹ thuật</p>
+                <p class="text-danger fw-bold mb-3">1800 6601</p>
+                <p class="fw-bold small mb-2">Hỗ trợ thanh toán</p>
+                <div>
+                    <img src="/public/assets/client/images/others/28.png" alt="Payment" class="img-fluid" style="max-width: 200px;">
                 </div>
             </div>
 
-            <div class="col l-2 m-4 c-12">
-                <div class="footer-2">
-                    <ul class="footer-list">
-                        <li class="footer-list-item"><a href="#">Tin tuyển dụng</a></li>
-                        <li class="footer-list-item"><a href="#">Tin khuyến mãi</a></li>
-                        <li class="footer-list-item"><a href="#">Hướng dẫn mua online</a></li>
-                        <li class="footer-list-item"><a href="#">Hướng dẫn mua trả góp</a></li>
-                        <li class="footer-list-item"><a href="#">Chính sách trả góp</a></li>
-                    </ul>
+            <div class="col-lg-3 col-md-6 col-12">
+                <p class="fw-bold small mb-1">Góp ý, khiếu nại dịch vụ (8h00-22h00)</p>
+                <p class="text-danger fw-bold mb-3">1800 6616</p>
+                <p class="fw-bold small mb-2">Chứng nhận:</p>
+                <div class="mb-3">
+                    <img src="/public/assets/client/images/others/29.png" alt="Certificate" class="img-fluid" style="max-width: 150px;">
                 </div>
-            </div>
-
-            <div class="col l-2 m-4 c-12">
-                <div class="footer-3">
-                    <ul class="footer-list">
-                        <li class="footer-list-item"><a href="#">Hệ thống cửa hàng</a></li>
-                        <li class="footer-list-item"><a href="#">Bán hàng doanh nghiệp</a></li>
-                        <li class="footer-list-item"><a href="#">Hệ thống bảo hành</a></li>
-                        <li class="footer-list-item"><a href="#">Giới thiệu máy đổi trả</a></li>
-                        <li class="footer-list-item"><a href="#">Chính sách đổi trả</a></li>
-                    </ul>
+                <p class="fw-bold small mb-2">Website cùng tập đoàn:</p>
+                <div>
+                    <img src="/public/assets/client/images/others/30.jpg" alt="Group" class="img-fluid" style="max-width: 150px;">
                 </div>
-            </div>
-
-            <div class="col l-3 m-6 c-12">
-                <div class="footer-4">
-                    <p class="title-footer">Tư vấn mua hàng (Miễn phí)</p>
-                    <p class="footer-phone">1800 6601</p>
-                    <p class="title-footer">Hỗ trợ kỹ thuật</p>
-                    <p class="footer-phone">1800 6601</p>
-                    <p class="title-footer">Hỗ trợ thanh toán</p>
-                    <div class="img-footer">
-                        <img src="/public/assets/client/images/others/28.png" alt="">
-                    </div>
-                </div>
-            </div>
-
-            <div class="col l-3 m-6 c-12">
-                <div class="footer-4">
-                    <p class="title-footer">Góp ý, khiếu nại dịch vụ (8h00-22h00)</p>
-                    <p class="footer-phone">1800 6616</p>
-                    <p class="title-footer">Chứng nhận:</p>
-                    <div class="img-footer footer-final">
-                        <img src="/public/assets/client/images/others/29.png" alt="">
-                    </div>
-                    <p class="title-footer">Website cùng tập đoàn:</p>
-                    <div class="img-footer footer-final">
-                        <img src="/public/assets/client/images/others/30.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<div class="footer-copyright">
-    <div class="grid wide">
-        <div class="row">
-            <div class="col l-12 m-12 c-12">
-                <p>© 2007 - 2022 Công Ty Cổ Phần Bán Lẻ Kỹ Thuật Số FPT / Địa chỉ: 261 - 263 Khánh Hội, P2, Q4,
-                    TP. Hồ Chí Minh / GPĐKKD số 0311609355 do Sở KHĐT TP.HCM cấp ngày 08/03/2012. GP số
-                    47/GP-TTĐT do sở TTTT TP HCM cấp ngày 02/07/2018. Điện thoại: (028)73023456. Email:
-                    fptshop@fpt.com.vn. Chịu trách nhiệm nội dung: Nguyễn Trịnh Nhật Linh.
-                </p>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="bg-white mt-4 py-3">
+        <div class="container-xl">
+            <p class="text-center text-muted small mb-0">
+                © 2007 - 2022 Công Ty Cổ Phần Bán Lẻ Kỹ Thuật Số FPT / Địa chỉ: 261 - 263 Khánh Hội, P2, Q4,
+                TP. Hồ Chí Minh / GPĐKKD số 0311609355 do Sở KHĐT TP.HCM cấp ngày 08/03/2012. GP số
+                47/GP-TTĐT do sở TTTT TP HCM cấp ngày 02/07/2018. Điện thoại: (028)73023456. Email:
+                fptshop@fpt.com.vn. Chịu trách nhiệm nội dung: Nguyễn Trịnh Nhật Linh.
+            </p>
+        </div>
+    </div>
+</footer>
