3737d04c8dccb643513a960e64077a3b7fd3c7fd
Đạt Trương <dat82770@gmail.com>
2026-03-30 13:09:11 +0700

    feat: add PHP mailer (#131)

--- public/index.php
+++ public/index.php
@@ -4,6 +4,12 @@ require_once '../app/core/Session.php';
 require_once '../config/db_module.php';
 require_once '../app/routes/client/client.php';
 require_once '../app/routes/admin/admin.php';
+require_once '../app/models/mailer/Exception.php';
+require_once '../app/models/mailer/PHPMailer.php';
+require_once '../app/models/mailer/SMTP.php';
+require_once '../app/core/functions.php';
+
+// sendMail('dattruong.31241024873@st.ueh.edu.vn', 'Test', 'Test');
 
 \App\Core\Session::start();
 
@@ -22,3 +28,5 @@ if ($path === 'admin' || strpos($path, 'admin/') === 0) {
 if ($link) {
     mysqli_close($link);
 }
+
+
