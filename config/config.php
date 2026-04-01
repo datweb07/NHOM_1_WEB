36615dffb094725ffae8f4801773136a0c172be2
datweb07 <dat82770@gmail.com>
2026-04-01 10:26:20 +0700

    feat: update services and config env

--- config/config.php
+++ config/config.php
@@ -5,4 +5,10 @@ define("DB", "db_web");
 define("USER", "root");
 define("PASSWORD", "123456");
 
+// define('HOST', (string) env('HOST', 'localhost'));
+// define('DB', (string) env('DB', 'db_web'));
+// define('PORT', (string) env('DB_PORT', '3306'));
+// define('USER', (string) env('USER', 'root'));
+// define('PASSWORD', (string) env('PASSWORD', '123456'));
+
 ?>
