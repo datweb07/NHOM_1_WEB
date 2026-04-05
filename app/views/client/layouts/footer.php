698e511251fe5a8e13c39a336ac7613e8754b714
Đạt Trương <dat82770@gmail.com>
2026-04-05 19:27:35 +0700

    feat: implement base footer layout for client pages (#185)

--- app/views/client/layouts/footer.php
+++ app/views/client/layouts/footer.php
@@ -1,71 +1,168 @@
-<footer class="bg-light pt-5 pb-3 mt-5">
+<footer class="footer-fpt mt-5">
     <div class="container-xl">
-        <div class="row g-4">
+        <div class="row g-4 mb-4">
+            <div class="col-lg-3 col-md-6 col-12">
+                <h5 class="footer-title">TẢI ỨNG DỤNG FPT SHOP</h5>
+                <div class="app-download mb-4">
+                    <img src="/public/assets/client/images/others/qr-fptshop.webp" alt="QR Code" class="qr-code">
+                    <div class="store-buttons">
+                        <a href="#"><img src="/public/assets/client/images/others/app-store.webp" alt="App Store"></a>
+                        <a href="#"><img src="/public/assets/client/images/others/google-play.webp"
+                                alt="Google Play"></a>
+                    </div>
+                </div>
 
-            <div class="col-lg-2 col-md-4 col-6">
-                <ul class="list-unstyled">
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Giới thiệu về công ty</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Câu hỏi thường gặp mua hàng</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Chính sách bảo mật</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Quy chế hoạt động</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Kiểm tra hóa đơn điện tử</a></li>
+                <h5 class="footer-title">KẾT NỐI VỚI FPT SHOP</h5>
+                <div class="social-icons mb-4">
+                    <a href="https://www.facebook.com/FPTShopOnline">
+                        <img src="https://cdn2.fptshop.com.vn/svg/facebook_icon_8543190720.svg" alt="Facebook"
+                            style="width:24px; height:24px;">
+                    </a>
+
+                    <a href="https://zalo.me/2659487666103055748">
+                        <img src="https://cdn2.fptshop.com.vn/svg/zalo_icon_8cbef61812.svg" alt="Zalo"
+                            style="width:24px; height:24px;">
+                    </a>
+
+                    <a href="https://www.youtube.com/@FPTShop_">
+                        <img src="https://cdn2.fptshop.com.vn/svg/youtube_icon_b492d61ba5.svg" alt="YouTube"
+                            style="width:24px; height:24px;">
+                    </a>
+
+                    <a href="https://www.tiktok.com/@fptshop.official">
+                        <img src="https://cdn2.fptshop.com.vn/svg/tiktok_icon_faabbeeb61.svg" alt="TikTok"
+                            style="width:24px; height:24px;">
+                    </a>
+                </div>
+
+                <h5 class="footer-title">TỔNG ĐÀI MIỄN PHÍ</h5>
+                <div class="hotline-item">
+                    <p>Tư vấn mua hàng (Miễn phí)</p>
+                    <p class="hotline-number">1800.6601 <span>(Nhánh 1)</span></p>
+                </div>
+                <div class="hotline-item">
+                    <p>Hỗ trợ kỹ thuật</p>
+                    <p class="hotline-number">1800.6601 <span>(Nhánh 2)</span></p>
+                </div>
+                <div class="hotline-item">
+                    <p>Góp ý, khiếu nại và tiếp nhận cảnh báo vi phạm</p>
+                    <p class="hotline-number">1800.6616 <span>(8h00 - 22h00)</span></p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 col-6">
+                <h5 class="footer-title">VỀ CHÚNG TÔI</h5>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="#">Giới thiệu về công ty</a></li>
+                    <li><a href="#">Quy chế hoạt động</a></li>
+                    <li><a href="#">Dự án Doanh nghiệp</a></li>
+                    <li><a href="#">Tin tức khuyến mại</a></li>
+                    <li><a href="#">Điện thoại chính hãng</a></li>
+                    <li><a href="#">Giới thiệu máy đổi trả</a></li>
+                    <li><a href="#">Hướng dẫn mua hàng & thanh toán online</a></li>
+                    <li><a href="#">Đại lý uỷ quyền và TTBH uỷ quyền của Apple</a></li>
+                    <li><a href="#">Tra cứu hoá đơn điện tử</a></li>
+                    <li><a href="#">Tra cứu bảo hành</a></li>
+                    <li><a href="#">Câu hỏi thường gặp</a></li>
                 </ul>
             </div>
 
-            <div class="col-lg-2 col-md-4 col-6">
-                <ul class="list-unstyled">
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Tin tuyển dụng</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Tin khuyến mãi</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Hướng dẫn mua online</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Hướng dẫn mua trả góp</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Chính sách trả góp</a></li>
-                </ul>
-            </div>
-
-            <div class="col-lg-2 col-md-4 col-6">
-                <ul class="list-unstyled">
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Hệ thống cửa hàng</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Bán hàng doanh nghiệp</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Hệ thống bảo hành</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Giới thiệu máy đổi trả</a></li>
-                    <li class="mb-2"><a href="#" class="text-decoration-none text-dark small">Chính sách đổi trả</a></li>
+            <div class="col-lg-3 col-md-6 col-6">
+                <h5 class="footer-title">CHÍNH SÁCH</h5>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="#">Chính sách bảo hành</a></li>
+                    <li><a href="#">Chính sách đổi trả</a></li>
+                    <li><a href="#">Chính sách bảo mật</a></li>
+                    <li><a href="#">Chính sách trả góp</a></li>
+                    <li><a href="#">Chính sách khui hộp sản phẩm</a></li>
+                    <li><a href="#">Chính sách giao hàng & lắp đặt</a></li>
+                    <li><a href="#">Chính sách mạng di động FPT</a></li>
+                    <li><a href="#">Chính sách bảo mật dữ liệu cá nhân</a></li>
+                    <li><a href="#">Quy định về hỗ trợ kỹ thuật & sao lưu dữ liệu</a></li>
+                    <li><a href="#">Chính sách giao hàng & lắp đặt Điện máy, Gia dụng</a></li>
+                    <li><a href="#">Chính sách chương trình khách hàng thân thiết</a></li>
                 </ul>
             </div>
 
             <div class="col-lg-3 col-md-6 col-12">
-                <p class="fw-bold small mb-1">Tư vấn mua hàng (Miễn phí)</p>
-                <p class="text-danger fw-bold mb-3">1800 6601</p>
-                <p class="fw-bold small mb-1">Hỗ trợ kỹ thuật</p>
-                <p class="text-danger fw-bold mb-3">1800 6601</p>
-                <p class="fw-bold small mb-2">Hỗ trợ thanh toán</p>
-                <div>
-                    <img src="/public/assets/client/images/others/28.png" alt="Payment" class="img-fluid" style="max-width: 200px;">
+                <h5 class="footer-title">HỖ TRỢ THANH TOÁN</h5>
+                <div class="payment-grid mb-4">
+                    <img src="https://cdn2.fptshop.com.vn/svg/visa_icon_44fe6e15ed.svg" alt="Visa" class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/mastercard_icon_c75f94f6a5.svg" alt="Mastercard"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/jcb_icon_214783937c.svg" alt="JCB" class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/amex_icon_d6fb68108d.svg" alt="Amex" class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/vnpay_icon_f42045057d.svg" alt="VNPAY" class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/zalopay_icon_26d64ea93f.svg" alt="ZaloPay"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/napas_icon_94d5330e3c.svg" alt="Napas" class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/kredivo_icon_04f72baf36.svg" alt="Kredivo"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/momo_icon_baef21b5f7.svg" alt="MoMo" class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/foxpay_icon_063b36c1f8.svg" alt="Foxpay"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/alepay_icon_20d5310617.svg" alt="Alepay"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/muadee_icon_5e297d9e61.svg" alt="Muadee"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/homepaylater_icon_adef600842.svg" alt="Home PayLater"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/applepay_icon_cb6806a0d0.svg" alt="Apple Pay"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/samsungpay_icon_0292aa9876.svg" alt="Samsung Pay"
+                        class="img-fluid">
+                    <img src="https://cdn2.fptshop.com.vn/svg/googlepay_icon_afa293cc14.svg" alt="Google Pay"
+                        class="img-fluid">
                 </div>
-            </div>
 
-            <div class="col-lg-3 col-md-6 col-12">
-                <p class="fw-bold small mb-1">Góp ý, khiếu nại dịch vụ (8h00-22h00)</p>
-                <p class="text-danger fw-bold mb-3">1800 6616</p>
-                <p class="fw-bold small mb-2">Chứng nhận:</p>
-                <div class="mb-3">
-                    <img src="/public/assets/client/images/others/29.png" alt="Certificate" class="img-fluid" style="max-width: 150px;">
-                </div>
-                <p class="fw-bold small mb-2">Website cùng tập đoàn:</p>
+                <h5 class="footer-title">CHỨNG NHẬN</h5>
                 <div>
-                    <img src="/public/assets/client/images/others/30.jpg" alt="Group" class="img-fluid" style="max-width: 150px;">
+                    <img src="https://cdn2.fptshop.com.vn/svg/dmca_icon_8fc6622bd5.svg" alt="Chứng nhận" class="img-fluid"
+                        style="max-width: 250px;">
+                            <img src="https://cdn2.fptshop.com.vn/svg/thuong_hieu_manh_2013_icon_b56f772475.svg" alt="Chứng nhận" class="img-fluid"
+                        style="max-width: 250px;">
+                       
+                            <img src="https://cdn2.fptshop.com.vn/svg/san_pham_dich_vu_hang_dau_viet_nam_icon_282a9ba4f7.svg" alt="Chứng nhận" class="img-fluid"
+                        style="max-width: 250px;">
+                               <img src="https://cdn2.fptshop.com.vn/svg/noi_khong_voi_hang_gia_icon_e16037d9cb.svg" alt="Chứng nhận" class="img-fluid"
+                        style="max-width: 250px;">
+                                     <img src="https://cdn2.fptshop.com.vn/svg/da_thong_bao_bo_cong_thuong_icon_64785fb3f7.svg" alt="Chứng nhận" class="img-fluid"
+                        style="max-width: 250px;">
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="bg-white mt-4 py-3">
-        <div class="container-xl">
-            <p class="text-center text-muted small mb-0">
-                © 2007 - 2022 Công Ty Cổ Phần Bán Lẻ Kỹ Thuật Số FPT / Địa chỉ: 261 - 263 Khánh Hội, P2, Q4,
-                TP. Hồ Chí Minh / GPĐKKD số 0311609355 do Sở KHĐT TP.HCM cấp ngày 08/03/2012. GP số
-                47/GP-TTĐT do sở TTTT TP HCM cấp ngày 02/07/2018. Điện thoại: (028)73023456. Email:
-                fptshop@fpt.com.vn. Chịu trách nhiệm nội dung: Nguyễn Trịnh Nhật Linh.
-            </p>
+        <div class="footer-middle">
+    <div class="website-label">WEBSITE CÙNG FPT RETAIL</div>
+    
+    <div class="partner-group">
+        <div class="partner-item">
+            <p>Cửa hàng uỷ quyền bởi Apple:</p>
+            <a href="https://fstudiobyfpt.com.vn/"><img src="https://cdn2.fptshop.com.vn/svg/fstudio_icon_97c012f7bf.svg" alt="F.Studio"></a>
+        </div>
+
+        <div class="partner-item">
+            <p>Trung tâm bảo hành uỷ quyền Apple:</p>
+            <a href="https://fstudiobyfpt.com.vn/fcare"><img src="https://cdn2.fptshop.com.vn/svg/fcare_icon_6a21e551f6.svg" alt="F.Care"></a>
+        </div>
+
+        <div class="partner-item">
+            <p>Chuỗi nhà thuốc:</p>
+            <a href="https://nhathuoclongchau.com.vn/"><img src="https://cdn2.fptshop.com.vn/svg/nha_thuoc_long_chau_icon_fc294f9f11.svg" alt="Nhà thuốc Long Châu"></a>
+        </div>
+    </div>
+</div>
+
+        <div class="footer-bottom">
+            <!-- <p class="seo-links">
+                <span class="text-white fw-medium">Sản phẩm tìm kiếm nhiều:</span>
+                <a href="#">Macbook Neo</a> | <a href="#">Samsung A37</a> | <a href="#">Samsung A57</a> | <a
+                    href="#">Samsung S25</a> | <a href="#">Samsung A56</a>
+            </p> -->
+            <p>© 2007 - 2024 Công Ty Cổ Phần Bán Lẻ Kỹ Thuật Số FPT • Địa chỉ: 261 - 263 Khánh Hội, P. Vĩnh Hội, TP. Hồ
+                Chí Minh • GPĐKKD số 0311609355 do Sở KHĐT TP.HCM cấp ngày 08/03/2012.</p>
+            <p>GP số 47/GP-TTĐT do sở TTTT TP HCM cấp ngày 02/07/2018 • Điện thoại: <strong>(028) 7302 3456</strong> •
+                Email: <strong>fptshop@fpt.com</strong> • Chịu trách nhiệm nội dung: Nguyễn Trịnh Nhật Linh.</p>
         </div>
     </div>
 </footer>
